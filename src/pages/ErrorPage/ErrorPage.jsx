c428d8a2bc4c7da9fcdb7de604fbab8aabf7b6cb
hirock6 <hirockdutta0@gmail.com>
2024-11-05 16:16:09 +0600

    errorpage completed

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
-const ErrorPage = () => {
-    return (
-        <div>ErrorPage</div>
-    )
-}
+import { Link } from "react-router-dom";
 
-export default ErrorPage
+const ErrorPage = () => {
+  return (
+    <main className="bg-gray-900">
+      <div className=" flex flex-col items-center justify-center min-h-screen  text-white">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-2xl mb-6 max-sm:text-base">
+          Oops! The page you're looking for doesn't exist.
+        </p>
+        <Link
+          to="/"
+          className="px-6 py-3 bg-blue-500 text-white rounded-lg text-lg hover:bg-blue-600 transition"
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default ErrorPage;
